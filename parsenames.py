1ee84bbfe4a3fb7f1e8519a82297050a51b230d6
saintak <saintak@users.noreply.github.com>
2025-03-21 12:21:35 +0800

    Update parsenames.py
    change to python3

--- parsenames.py
+++ parsenames.py
@@ -8,7 +8,7 @@ def get_folder_files(folder, extension):
     return [join(folder,f) for f in listdir(folder)
         if isfile(join(folder,f)) and f.endswith(extension)]
 
-print 'starting...'
+print('starting...')
 names_set = set()
 filenames = get_folder_files('names', '.txt')
 for fn in filenames:
@@ -18,9 +18,11 @@ for fn in filenames:
         names = [n for n in names if len(n)>NAME_MIN_LENGTH] # name length filter
         names_set.update(names)
 
-print len(names_set), 'unique names'
+print(len(names_set), 'unique names')
 with open('names-dataset.txt', 'w+') as f:
-    f.write('\n'.join(names_set).lower())
-print 'file written'
-print '...finished'
+    f.write('\
+'.join(names_set).lower() + '\
+')
+print('file written')
+print('...finished')
 
